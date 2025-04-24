d7af5de0109268b4fd37cef38c6c4e80ef484a2e
tiagovitorino97 <tiagomsvitorino@gmail.com>
2025-04-24 12:32:03 +0100

    v1.2.0
    - Changed assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
 
 
-[assembly: MelonInfo(typeof(LabelMod), "SimpleLabels", "1.1.2", "Tiago")]
+[assembly: MelonInfo(typeof(LabelMod), "SimpleLabels", "1.2.0", "Tiago")]
 [assembly: MelonGame("TVGS", "Schedule I")]
 //[assembly: MelonOptionalDependencies("FishNet.Runtime")]
 // General Information about an assembly is controlled through the following
